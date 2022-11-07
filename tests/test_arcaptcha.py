ee1b352cd1d22d85896471d9661134c6215b57da
mahdi <mahdifarzadi78@gmail.com>
2022-11-07 12:41:34 +0330

    add test verify unit test

--- tests/test_arcaptcha.py
+++ tests/test_arcaptcha.py
@@ -9,6 +9,10 @@ class TestArcaptchaModule(unittest.TestCase):
     self.good_response = "{\"success\": true}"
     self.bad_response = "{\"status\": 404, \"message\": \"Challenge not exist\"}"
     self.captcha = Arcaptcha.Captcha(site_key=self.site_key, secret_key=self.secret_key)
+    self.test_site_key = "0000000000"
+    self.test_secret_key = "00000000000000000000"
+    self.test_response = "000000000000000000000000000000"
+    self.test_captcha = Arcaptcha.Captcha(site_key=self.test_site_key, secret_key=self.test_secret_key)
 
   def test_good_response(self):
     good_response = self.captcha.validate_response(self.good_response)
@@ -25,6 +29,13 @@ class TestArcaptchaModule(unittest.TestCase):
   def test_display(self):
     captcha = self.captcha.display()
     self.assertTrue(len(captcha) > 0)
+
+  def test_verify(self):
+    verified = self.test_captcha.verify(self.test_response)
+    self.assertTrue(verified, "Expected True but got Flase")
+    
+    verified = self.test_captcha.verify("invalid_response")
+    self.assertFalse(verified, "Expected False but got True")
 
 if __name__ == '__main__':
     unittest.main()
